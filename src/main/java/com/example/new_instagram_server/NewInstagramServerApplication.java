64ea593be10550c297794c4a45a9ff5ccf66ccc0
CordHouse <qkqktl5310@gmail.com>
2024-02-05 16:47:41 +0900

    Initial commit

---
+++ src/main/java/com/example/new_instagram_server/NewInstagramServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_instagram_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewInstagramServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewInstagramServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/new_instagram_server/NewInstagramServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_instagram_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewInstagramServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
