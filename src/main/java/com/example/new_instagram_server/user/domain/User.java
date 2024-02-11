df65fbd2acfbf7ac18c4cc163e1ef0657c3fd727
CordHouse <qkqktl5310@gmail.com>
2024-02-12 05:31:52 +0900

    #1 refactor : 비밀번호 컬럼 추가

--- src/main/java/com/example/new_instagram_server/user/domain/User.java
+++ src/main/java/com/example/new_instagram_server/user/domain/User.java
@@ -19,6 +19,9 @@ public class User {
     private long id;
 
     @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String nickname;
 
     @Column(nullable = false)
