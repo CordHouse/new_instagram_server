cd9a95f6aec1d111369744a204a7084532cb01b2
CordHouse <qkqktl5310@gmail.com>
2024-02-12 05:24:37 +0900

    #7 feat : 로그인 타임 아웃, 비밀번호 불일치 예외처리 추가

---
+++ src/main/java/com/example/new_instagram_server/user/advice/UserExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.new_instagram_server.user.advice;
+
+import com.example.new_instagram_server.user.advice.exception.LoginTimeOutException;
+import com.example.new_instagram_server.user.advice.exception.PasswordMismatchException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+    @ExceptionHandler(LoginTimeOutException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String loginTimeOutException() {
+        return "다시 로그인해 주세요.";
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String passwordMismatchException() {
+        return "비밀번호가 일치하지 않습니다.";
+    }
+}
