5c6a8b29670dad8fb35e378be57c946eff0c132d
CordHouse <qkqktl5310@gmail.com>
2024-02-20 03:52:51 +0900

    #1 refactor : 빈 등록 누락 추가

--- src/main/java/com/example/new_instagram_server/user/adapter/out/GetAuthenticationImpl.java
+++ src/main/java/com/example/new_instagram_server/user/adapter/out/GetAuthenticationImpl.java
@@ -3,7 +3,9 @@ package com.example.new_instagram_server.user.adapter.out;
 import com.example.new_instagram_server.user.application.port.out.GetAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GetAuthenticationImpl implements GetAuthentication {
     @Override
     public Authentication getAuthentication() {
