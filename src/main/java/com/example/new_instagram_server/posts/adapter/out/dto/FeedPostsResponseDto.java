e203f80eaf32ed220cf1924f19df924f2d367690
CordHouse <qkqktl5310@gmail.com>
2024-03-07 03:25:01 +0900

    #31 refactor : 변수 수정 카멜케이스

--- src/main/java/com/example/new_instagram_server/posts/adapter/out/dto/FeedPostsResponseDto.java
+++ src/main/java/com/example/new_instagram_server/posts/adapter/out/dto/FeedPostsResponseDto.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class FeedPostsResponseDto {
     private long id;
     private String content;
-    private String image_url;
+    private String imageUrl;
     private List<FeedCommentResponseDto> comments;
 
     public FeedPostsResponseDto toDo(Posts posts) {
