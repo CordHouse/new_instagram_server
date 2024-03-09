258cc643e4a1cac2d54786bfb66eb32ce850bd7a
CordHouse <qkqktl5310@gmail.com>
2024-03-09 19:52:10 +0900

    #31 feat : 게시글 생성, 수정, 삭제, 조회에 대한 비즈니스 로직 구현 및 페이징 적용

--- src/main/java/com/example/new_instagram_server/posts/adapter/in/dto/PostsEditRequestDto.java
+++ src/main/java/com/example/new_instagram_server/posts/adapter/in/dto/PostsEditRequestDto.java
@@ -3,6 +3,7 @@ package com.example.new_instagram_server.posts.adapter.in.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -15,6 +16,7 @@ public class PostsEditRequestDto {
     @NotNull(message = "수정할 게시글을 선택해주세요.")
     private long id;
 
+    @Nullable
     private MultipartFile image;
 
     @NotBlank(message = "수정할 내용을 입력해주세요.")
